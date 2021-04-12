a89b74ac9b8e2117a3a8c4f8ac7eeed6b68c4fc6
BlitzOffline <rpopus20@gmail.com>
2021-04-12 16:59:30 +0300

    Fixed bounty expiration runnable

--- src/main/kotlin/com/blitzoffline/bountysystem/runnable/SaveCache.kt
+++ src/main/kotlin/com/blitzoffline/bountysystem/runnable/SaveCache.kt
@@ -3,6 +3,4 @@ package com.blitzoffline.bountysystem.runnable
 import com.blitzoffline.bountysystem.BountySystem
 import org.bukkit.scheduler.BukkitRunnable
 
-class SaveCache(private val plugin: BountySystem) : BukkitRunnable() {
-    override fun run() = plugin.database.save()
-}
+class SaveCache(private val plugin: BountySystem) : BukkitRunnable() { override fun run() = plugin.database.save() }
