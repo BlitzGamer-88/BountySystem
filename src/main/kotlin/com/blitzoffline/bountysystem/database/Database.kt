51f62ad13b1a089795b28bd2053c7af07847e31e
BlitzOffline <rpopus20@gmail.com>
2021-04-14 03:12:11 +0300

    Added info messages when the bounties are loaded

--- src/main/kotlin/com/blitzoffline/bountysystem/database/Database.kt
+++ src/main/kotlin/com/blitzoffline/bountysystem/database/Database.kt
@@ -3,6 +3,7 @@ package com.blitzoffline.bountysystem.database
 import com.blitzoffline.bountysystem.BountySystem
 import com.blitzoffline.bountysystem.bounty.BOUNTIES_LIST
 import com.blitzoffline.bountysystem.bounty.Bounty
+import com.blitzoffline.bountysystem.util.log
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import java.util.logging.Level
@@ -14,22 +15,25 @@ class Database(private val plugin: BountySystem) {
         .create()
 
     fun load() {
+        var count = 0
         try {
+            "[BountySystem] Loading bounties...".log()
             val file = plugin.dataFolder.resolve("bounties.json")
 
             if (!file.exists()) plugin.dataFolder.resolve("bounties.json").createNewFile()
-            if (file.length() == 0L) return
+            if (file.length() == 0L) { "[BountySystem] Bounty file is empty. Loaded 0 bounties.".log(); return }
 
             val token = object : TypeToken<Collection<Bounty>>() {}.type
             val bounties: Collection<Bounty> = gson.fromJson(plugin.dataFolder.resolve("bounties.json").readText(), token)
 
             BOUNTIES_LIST.clear()
 
-            var count = 0
             bounties.forEach {  bounty ->
+                if (BOUNTIES_LIST[bounty.id.toString()] == null) count++
                 BOUNTIES_LIST[bounty.id.toString()] = bounty
-                count++
             }
+            if (bounties.size == count) "[BountySystem] Found and loaded $count bounties.".log()
+            else "[BountySystem] Found ${bounties.size} bounties but could only load $count of them.".log()
         }
         catch (ex: java.lang.Exception) {
             plugin.logger.log(Level.SEVERE, "Could not load the saved bounties!", ex)
