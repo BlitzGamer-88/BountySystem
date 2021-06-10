1bc0eb9a8efbfa68ce3068bc569f6bf6b6e95184
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2021-06-11 01:34:14 +0300

    Made onRequest an expression function

--- src/main/kotlin/com/blitzoffline/bountysystem/placeholders/BountyPlaceholders.kt
+++ src/main/kotlin/com/blitzoffline/bountysystem/placeholders/BountyPlaceholders.kt
@@ -8,43 +8,42 @@ class BountyPlaceholders(private val plugin: BountySystem) : PlaceholderExpansio
 
     override fun getIdentifier() = plugin.description.name.lowercase()
 
-    override fun getAuthor() = plugin.description.authors[0] ?: "BlitzOffline"
-
     override fun getVersion() = plugin.description.version
+
+    override fun getAuthor() = "BlitzOffline"
 
     override fun canRegister() = true
 
     override fun persist() = true
 
-    override fun onRequest(player: OfflinePlayer?, params: String): String? {
-        if (params.split("_").size != 2) return null
-
+    override fun onRequest(player: OfflinePlayer?, params: String) =
         when {
+            params.split("_").size != 2 -> null
 
             params.startsWith("target_", true) -> {
                 val id = params.substringAfter("target_")
-                val bounty = plugin.bountyHandler.BOUNTIES.firstOrNull { it.id.toString() == id && !plugin.bountyHandler.expired(it) } ?: return ""
-                return bounty.target().name ?: ""
+                val bounty = plugin.bountyHandler.BOUNTIES.firstOrNull { it.id.toString() == id && !plugin.bountyHandler.expired(it) } ?: run { return "" }
+                bounty.target().name ?: ""
             }
 
             params.startsWith("payer_", true) -> {
                 val id = params.substringAfter("payer_")
-                val bounty = plugin.bountyHandler.BOUNTIES.firstOrNull { it.id.toString() == id && !plugin.bountyHandler.expired(it) } ?: return ""
-                return bounty.payer().name ?: ""
+                val bounty = plugin.bountyHandler.BOUNTIES.firstOrNull { it.id.toString() == id && !plugin.bountyHandler.expired(it) } ?: run { return "" }
+                bounty.payer().name ?: ""
             }
 
             params.startsWith("amount_", true) -> {
                 val id = params.substringAfter("amount_")
-                val bounty = plugin.bountyHandler.BOUNTIES.firstOrNull { it.id.toString() == id && !plugin.bountyHandler.expired(it) } ?: return ""
-                return bounty.amount.toString()
+                val bounty = plugin.bountyHandler.BOUNTIES.firstOrNull { it.id.toString() == id && !plugin.bountyHandler.expired(it) } ?: run { return "" }
+                bounty.amount.toString()
             }
 
             params.startsWith("ids_", true) -> {
-                val p = plugin.server.getOfflinePlayerIfCached(params.substringAfter("ids_")) ?: return ""
+                val p = plugin.server.getOfflinePlayerIfCached(params.substringAfter("ids_")) ?: run { return "" }
                 val ids = plugin.bountyHandler.BOUNTIES.filter { !plugin.bountyHandler.expired(it) && it.payer() != p }.map { it.id }.ifEmpty { return "" }
-                return ids.joinToString(", ")
+                ids.joinToString(", ")
             }
+
+            else -> null
         }
-        return null
-    }
 }
